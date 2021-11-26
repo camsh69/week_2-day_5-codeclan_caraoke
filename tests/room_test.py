2c83606046897d67ea84a79efce42ce4d340bdb6
Campbell Forsyth <camsh69@gmail.com>
2021-11-26 13:03:35 +0000

    test for song is not in list commit

--- tests/room_test.py
+++ tests/room_test.py
@@ -8,6 +8,7 @@ class TestRoom(unittest.TestCase):
     def setUp(self):
         self.song_1 = Song("I will survive")
         self.song_2 = Song("Sweet Caroline")
+        self.song_3 = Song("Yesterday")
         self.song_list = [self.song_1, self.song_2]
         self.guest_1 = Guest("Joe Bloggs")
         self.guest_2 = Guest("Jane Doe")
@@ -18,7 +19,10 @@ class TestRoom(unittest.TestCase):
         self.assertEqual("Golden Oldie Room", self.room.name)
 
     def test_for_song_in_song_list(self):
-        self.assertEqual(True, self.room.find_song("Sweet Caroline"))
+        self.assertEqual(True, self.room.find_song(self.song_1))
+
+    def test_for_song_is_not_in_list(self):
+        self.assertEqual(False, self.room.find_song(self.song_3))
 
     def test_number_of_guests_in_room(self):
         self.assertEqual(2, self.room.guest_count())
